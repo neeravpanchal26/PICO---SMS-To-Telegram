4d0ce9f6e894cc43047ff0c23d97fad1866d0b9b
Neerav Panchal <neeravpanchal26@gmail.com>
2023-10-01 18:57:53 +0530

    [01-10-23] Class based OOP done.

--- main.py
+++ main.py
@@ -2,106 +2,77 @@ from machine import UART, Pin
 import time
 import ujson
 
-with open('config.json') as config_file:
-    config_json = ujson.load(config_file)
 
-# Configure UART
-uart = UART(0, baudrate=config_json['BAUDRATE'], tx=Pin(0), rx=Pin(1))
+class UARTCommunicator:
+    def __init__(self, config_file_path='config.json'):
+        with open(config_file_path) as config_file:
+            self.config_json = ujson.load(config_file)
+        self.uart = UART(
+            0, baudrate=self.config_json['BAUDRATE'], bits=8, parity=None, stop=1, tx=Pin(0), rx=Pin(1))
 
+        time.sleep(10)
+        response = self._exe_command('AT\r\n')
+        response += self._exe_command('ATE\r\n')
+        response += self._exe_command('ATI\r\n')
+        response += self._exe_command('AT+CIMI\r\n')
+        response += self._exe_command('AT+CNUM\r\n')
+        response += self._exe_command('AT+COPS?\r\n')
+        self.send_telegram_message(response)
 
-def send_telegram_message(message):
-    payload = {
-        "chat_id": config_json['CHAT_ID'],
-        "text": message,
-        "parse_mode": "HTML"
-    }
-    json_data = ujson.dumps(payload)
-    data_length = len(json_data)
+        while True:
+            sms_response = self.read_sms()
+            sms_messages = sms_response.decode('utf-8').split('+CMGL: ')[1:]
+            print(sms_messages)
+            for sms_message in sms_messages:
+                message_lines = sms_message.split('\r\n')
+                timestamp = message_lines[0].split(',')[4].replace('"', '')
+                message = message_lines[1]
+                formatted_message = "<b>Time: </b>{}\r\n<b>Message: </b>{}".format(
+                    timestamp, message)
+                self.send_telegram_message(formatted_message)
+                sms_index = message_lines[0].split(',')[0]
+                self.uart.write(
+                    'AT+CMGD={}\r\n'.format(sms_index))
+                time.sleep(1)
+            self._exe_command('AT+CMGD=1,4\r\n')
+            time.sleep(10)
 
-    uart.write('AT+CSQ\r\n')
-    time.sleep(2)
-    response = uart.read()
-    print(response)
+    def _exe_command(self, command):
+        self.uart.write(command)
+        time.sleep(2)
+        response = self.uart.read()
+        print(response.decode('utf-8'))
+        return response
 
-    uart.write('AT+CREG?\r\n')
-    time.sleep(2)
-    response = uart.read()
-    print(response)
+    def send_telegram_message(self, message):
+        payload = {
+            "chat_id": self.config_json['CHAT_ID'],
+            "text": message,
+            "parse_mode": "HTML"
+        }
+        json_data = ujson.dumps(payload)
+        data_length = len(json_data)
 
-    uart.write('AT+CGDCONT=1,"IP","'+config_json['APN']+'"\r\n')
-    time.sleep(2)
-    response = uart.read()
-    print(response)
+        uart_commands = [
+            'AT+HTTPINIT\r\n',
+            'AT+HTTPPARA="URL","https://api.telegram.org/bot' +
+            self.config_json['BOT_TOKEN'] + '/sendMessage"\r\n',
+            'AT+HTTPPARA="CONTENT","application/json"\r\n',
+            'AT+HTTPDATA={},10000\r\n'.format(data_length),
+            json_data,
+            'AT+HTTPACTION=1\r\n',
+            'AT+HTTPHEAD\r\n',
+            'AT+HTTPTERM\r\n'
+        ]
 
-    uart.write('AT+CGACT=1,1\r\n')
-    time.sleep(2)
-    response = uart.read()
-    print(response)
+        for command in uart_commands:
+            self._exe_command(command)
 
-    uart.write('AT+HTTPINIT\r\n')
-    time.sleep(2)
-    response = uart.read()
-    print(response)
-
-    uart.write('AT+HTTPPARA="URL","https://api.telegram.org/bot' +
-               config_json['BOT_TOKEN']+'/sendMessage"\r\n')
-    time.sleep(2)
-    response = uart.read()
-    print(response)
-
-    uart.write('AT+HTTPPARA="CONTENT","application/json"\r\n')
-    time.sleep(2)
-    response = uart.read()
-    print(response)
-
-    uart.write('AT+HTTPDATA={},10000\r\n'.format(data_length))
-    time.sleep(2)
-    uart.write(json_data)
-    time.sleep(2)
-    print(response)
-
-    uart.write('AT+HTTPACTION=1\r\n')
-    time.sleep(2)
-    response = uart.read()
-    print(response)
-
-    uart.write('AT+HTTPHEAD\r\n')
-    time.sleep(2)
-    response = uart.read()
-    print(response)
-
-    uart.write('AT+HTTPTERM\r\n')
-    time.sleep(2)
-    response = uart.read()
-    print(response)
+    def read_sms(self):
+        self._exe_command('AT+CMGF=1\r\n')
+        return self._exe_command('AT+CMGL="REC UNREAD"\r\n')
 
 
-def read_sms():
-    uart.write('AT+CMGF=1\r\n')
-    time.sleep(1)
-    uart.write('AT+CMGL="REC UNREAD"\r\n')
-    time.sleep(2)
-    response = uart.read()
-    return response
+if __name__ == "__main__":
+    UARTCommunicator()
 
-
-while True:
-    sms_response = read_sms()
-    sms_messages = sms_response.decode('utf-8').split('+CMGL: ')[1:]
-    print(sms_messages)
-    for sms_message in sms_messages:
-        message_lines = sms_message.split('\r\n')
-        number = message_lines[0].split(',')[2].replace('"', '')
-        timestamp = message_lines[0].split(',')[4].replace('"', '')
-        message = message_lines[1]
-        formatted_message = "<b>From:</b> {}\n<b>Time:</b> {}\n<b>Message:</b> {}".format(
-            number, timestamp, message)
-        send_telegram_message(formatted_message)
-        sms_index = message_lines[0].split(',')[0]
-        uart.write('AT+CMGD={}\r\n'.format(sms_index))
-        time.sleep(1)
-    uart.write('AT+CMGD=1,4\r\n')
-    time.sleep(2)
-    response = uart.read()
-    print(response)
-    time.sleep(10)
